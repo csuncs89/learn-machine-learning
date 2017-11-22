94b52ca083ee3a09e5fab563ca419c261458cd42
csuncs89 <csuncs89@gmail.com>
2017-11-22 12:26:57 +0800

    Refactor code

--- p1.py
+++ p1.py
@@ -5,8 +5,7 @@ from sklearn import datasets
 from sklearn import svm
 
 
-def main():
-    iris = datasets.load_iris()
+def learn_and_predict():
     digits = datasets.load_digits()
 
     print(digits.data)
@@ -14,7 +13,7 @@ def main():
     classifier = svm.SVC(gamma=0.001, C=100.)
     print(classifier)
 
-    num_training = len(digits.data) - 1
+    num_training = len(digits.data) - 5
     X_training = digits.data[:num_training]
     ys_training = digits.target[:num_training]
     X_test = digits.data[num_training:]
@@ -26,11 +25,17 @@ def main():
 
     for i_pred, (y_pred, y_test) in enumerate(zip(ys_pred, ys_test)):
         if y_pred == y_test:
-            print('  Correct prediction (%d) for sample %d (%d)' %
-                  (y_pred, i_pred + 1, y_test))
+            print('Test sample {i_sample} ({y_test}) is  CORRECTLY '
+                  ' predicted as {y_pred}'
+                  .format(y_pred=y_pred, i_sample=i_pred + 1, y_test=y_test))
         else:
-            print('Incorrect prediction (%d) for sample %d (%d)' %
-                  (y_pred, i_pred + 1, y_test))
+            print('Test sample {i_sample} ({y_test}) is INCORRECTLY'
+                  ' predicted as {y_pred}'
+                  .format(y_pred=y_pred, i_sample=i_pred + 1, y_test=y_test))
+
+
+def main():
+    learn_and_predict()
 
 
 if __name__ == '__main__':
