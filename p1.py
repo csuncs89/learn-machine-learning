8c9a1d9d39f38267da7e1abd1c04336b41154181
csuncs89 <csuncs89@gmail.com>
2017-11-22 12:46:32 +0800

    Model persistence

--- p1.py
+++ p1.py
@@ -1,8 +1,19 @@
 """
 
 """
+import pickle
+
 from sklearn import datasets
 from sklearn import svm
+
+
+def split_dataset(X, ys, num_training):
+    X_training = X[:num_training]
+    ys_training = ys[:num_training]
+    X_test = X[num_training:]
+    ys_test = ys[num_training:]
+
+    return (X_training, ys_training), (X_test, ys_test)
 
 
 def learn_and_predict():
@@ -14,10 +25,8 @@ def learn_and_predict():
     print(classifier)
 
     num_training = len(digits.data) - 5
-    X_training = digits.data[:num_training]
-    ys_training = digits.target[:num_training]
-    X_test = digits.data[num_training:]
-    ys_test = digits.target[num_training:]
+    (X_training, ys_training), (X_test, ys_test) = \
+        split_dataset(digits.data, digits.target, num_training)
 
     classifier.fit(X=X_training, y=ys_training)
     ys_pred = classifier.predict(X_test)
@@ -34,8 +43,29 @@ def learn_and_predict():
                   .format(y_pred=y_pred, i_sample=i_pred + 1, y_test=y_test))
 
 
+def model_persistence():
+    classifier = svm.SVC()
+
+    iris = datasets.load_iris()
+    (X_training, ys_training), (X_test, ys_test) = \
+        split_dataset(iris.data, iris.target, num_training=len(iris.data) - 5)
+
+    classifier.fit(X_training, ys_training)
+
+    pkl_classifier = pickle.dumps(classifier)
+
+    classifier2 = pickle.loads(pkl_classifier)
+    ys_pred2 = classifier2.predict(X_test)
+
+    ys_pred1 = classifier.predict(X_test)
+
+    print ys_pred1 == ys_pred2
+    print ys_pred1 == ys_test
+
+
 def main():
     learn_and_predict()
+    model_persistence()
 
 
 if __name__ == '__main__':
