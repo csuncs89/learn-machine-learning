35131b3cc7440b49f98726079073679a136e0c07
csuncs89 <csuncs89@gmail.com>
2017-12-06 18:43:16 +0800

    Add l1 regularizer recognizer

--- tasks/dzj/tune_regularizer.py
+++ tasks/dzj/tune_regularizer.py
@@ -172,6 +172,51 @@ class DzjRecognizerL2RegConv(DzjRecognizerBaseline):
         self._model = model
 
 
+class DzjRecognizerL1Reg(DzjRecognizerBaseline):
+
+    def _configure(self):
+        super(DzjRecognizerL1Reg, self)._configure()
+        self.version_recognizer = 'l1_reg'
+
+    def _create_model(self):
+        """
+        3x3x32 3x3x64 2x2 3x3x64 2x2 500 500 200
+        """
+        input_shape = (self.h_img, self.w_img, 1)
+
+        model = models.Sequential()
+
+        model.add(layers.Conv2D(32, (3, 3), input_shape=input_shape))
+        model.add(layers.Activation('relu'))
+
+        model.add(layers.Conv2D(64, (3, 3)))
+        model.add(layers.Activation('relu'))
+
+        model.add(layers.MaxPool2D(pool_size=(2, 2)))
+
+        model.add(layers.Conv2D(64, (3, 3)))
+        model.add(layers.Activation('relu'))
+
+        model.add(layers.MaxPool2D(pool_size=(2, 2)))
+
+        model.add(layers.Flatten())
+
+        model.add(layers.Dense(500, kernel_regularizer=regularizers.l1(0.01)))
+        model.add(layers.Activation('relu'))
+
+        model.add(layers.Dense(500, kernel_regularizer=regularizers.l1(0.01)))
+        model.add(layers.Activation('relu'))
+
+        model.add(layers.Dense(self.num_classes))
+        model.add(layers.Activation('softmax'))
+
+        model.compile(loss=keras.losses.categorical_crossentropy,
+                      optimizer=keras.optimizers.Adadelta(),
+                      metrics=['accuracy'])
+        self._model = model
+
+
+
 def main():
     args = parse_args()
 
